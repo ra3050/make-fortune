412b152167e9df68464bc7d36f19cd895125799c
ra3050 <hek33kr@gmail.com>
2024-11-26 19:50:00 +0900

    feat: Axios Post타입 추가

--- src/lib/api/client.tsx
+++ src/lib/api/client.tsx
@@ -34,6 +34,57 @@ export const axiosGetForm = async <T,>(
   }
 };
 
+export const axiosPostForm = async <T,>(
+  uri: string,
+  body?: Record<any, string>,
+  contentType?: string,
+  authCheck: boolean = false,
+  otherAxiosConfig?: any
+): Promise<AxiosResponse<T> | void | undefined> => {
+  let checkTokenFlag = true;
+  // if "체크토큰"
+  // not if "체크토큰"  :: 에러로그 + 리턴 undifined
+
+  if (!reqList.find((v: string): boolean => v === uri)) {
+    reqList.push(uri);
+
+    let header = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "",
+    };
+    if (contentType) {
+      header["Content-Type"] = contentType;
+    }
+    if (authCheck) {
+      header["Authorization"] =
+        "Bearar" + "내부저장소에 저장된 엑세스 토큰을 발급받으세요";
+    }
+
+    const axiosConfig = {
+      ...otherAxiosConfig,
+      headers: {
+        ...header,
+      },
+    };
+
+    let response = undefined;
+    try {
+      response = await client.post(uri, body, axiosConfig);
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        // 에러처리
+      }
+
+      console.log(uri, ":: axiosPostForm Error  :: ", e);
+    } finally {
+      reqList = reqList.filter((v: string) => {
+        return v != uri;
+      });
+    }
+    return response;
+  }
+};
+
 // AxiosPostForm :: header값을 수동으로 config: 'application/json'을 작성해서 넣어줄 수 있도록 합니다.
 
 // 2024.11.19 :: Promise<AxiosResponse<T>> 여기에 적용하고 사용할 수 있을까?
